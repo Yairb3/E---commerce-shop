1e599aa8edf670b75cf1ec5b2b77fb82df5f89f6
Yair Ben Michael <92392364+Yairb3@users.noreply.github.com>
2023-04-11 12:22:36 +0300

    src

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
+import { Switch, Route } from "react-router-dom";
+import Products from "./component/Products";
+import Login from "./component/Login";
+import Product from "./component/Product";
+import Cart from "./component/Cart";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/products" component={Login} />
+        <Route exact path="/products/:id" component={Product} />
+        <Route exact path="/cart" component={Cart} />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
