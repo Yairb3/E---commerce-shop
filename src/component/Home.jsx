c5e7ab316575dc598c184fffe2c072573a66c3e0
Roee Negri <roee.negri@salto.io>
2023-04-22 12:48:11 +0300

    mychange

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <img src="/assets/bg.jpg" className="card-img" alt="Background" height="550px" />
         <div className="card-img-overlay d-flex flex-column justify-content-center">
             <div className="container">
-          <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+          <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS!</h5>
           <p className="card-text lead fs-2">
             CHECK OUT ALL THE TRENDS
           </p>
