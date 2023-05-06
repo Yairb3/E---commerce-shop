0c0ee2339ae940b3b184f4a5f86acb37021f55e2
Yair Ben Michael <92392364+Yairb3@users.noreply.github.com>
2023-05-06 22:33:12 +0300

    update contact

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,55 +1,49 @@
 import React from 'react';
+import './Contact.css';
 const Contact = () => {
 
 
 
 
     return (
-        <div className="container blackbackground">
-        <div className="content">
-          <div className="left-side">
-            <div className="address details">
-              <i className="fas fa-map-marker-alt"></i>
-              <div className="topic">Address</div>
-              <div className="text-one">Gergo Wise, Tel Aviv</div>
-            </div>
-            <div className="phone details">
-              <i className="fas fa-phone-alt"></i>
-              <div className="topic">Phone</div>
-              <div className="text-one">0543267377</div>
-              <div className="text-two">+0096 3434 5678</div>
-            </div>
-            <div className="email details">
-              <i className="fas fa-envelope"></i>
-              <div className="topic">Email</div>
-              <div className="text-one">yuvalramati@gmail.com</div>
-              <div className="text-one">yairbenmichael@gmail.com</div>
-              <div className="text-one">roeenegri@gmail.com</div>
-              <div className="text-one">yuvalleibovich@gmail.com</div>
-              <div className="text-one">shaharblitzer@gmail.com</div>
-              
-            </div>
+      <div class="containerr">
+      <div class="content">
+        <div class="left-side">
+          <div class="address details">
+            <i class="fas fa-map-marker-alt"></i>
+            <div class="topic">Address</div>
+            <div class="text-one">George Wise, Tel Aviv</div>
           </div>
-          <div className="right-side">
-            <div className="topic-text">Send us a message</div>
-            <p>If you have any work from me or any types of quries related to my tutorial, you can send me message from here. It's my pleasure to help you.</p>
-          <form action="#">
-            <div className="input-box">
-              <input type="text" placeholder="Enter your name"></input>
-            </div>
-            <div className="input-box">
-              <input type="text" placeholder="Enter your email"></input>
-            </div>
-            <div className="input-box message-box">
-              
-            </div>
-            <div className="button">
-              <input type="button" value="Send Now" ></input>
-            </div>
-          </form>
+          <div class="phone details">
+            <i class="fas fa-phone-alt"></i>
+            <div class="topic">Phone</div>
+            <div class="text-one">0543267377</div>
+          </div>
+          <div class="email details">
+            <div class="topic">Email</div>
+            <div class="text-one">yuval.leibovici@gmail.com</div>
+          </div>
         </div>
-        </div>
+        <div class="right-side">
+          <div class="topic-text">Send us a message</div>
+          <p>It's our pleasure to help you!</p>
+        <form action="#">
+          <div class="input-box">
+            <input type="text" placeholder="Enter your name"></input>
+          </div>
+          <div class="input-box">
+            <input type="text" placeholder="Enter your email"></input>
+          </div>
+          <div class="input-box message-box">
+            
+          </div>
+          <div class="button">
+            <input type="button" value="Send Now" ></input>
+          </div>
+        </form>
       </div>
+      </div>
+    </div>
     )
 
 
