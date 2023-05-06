4b2636ec368075a693c5131099fb0a2f19df7fad
Yair Ben Michael <92392364+Yairb3@users.noreply.github.com>
2023-05-06 22:38:29 +0300

    update products design

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -59,12 +59,12 @@ const Products = () => {
       </>
     );
   };
-  return (
+  return ( 
     <div className="blackbackground" >
       <div className="container my-5 py-5 ">
         <div className="row">
           <div className="col-12 mb-5">
-            <h1 className="display-6 fw-bolder text-center ">Latest Products</h1>
+            <h1 className="text-blk heading text-center ">Latest Products</h1>
             <hr />
           </div>
         </div>
