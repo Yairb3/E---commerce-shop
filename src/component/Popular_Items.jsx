bf4aa7aa021299878f7b36fa67a65424d3e16115
Yair Ben Michael <92392364+Yairb3@users.noreply.github.com>
2023-05-15 12:48:32 +0300

    Add popularity algorithm

---
+++ src/component/Popular_Items.jsx
@@ -0,0 +1,62 @@
+import React, { useContext } from 'react';
+import DataContext from './usedb';
+import Items from './Items';
+import { NavLink } from "react-router-dom";
+
+const PopularItems = () => {
+  const { ratings, data } = useContext(DataContext);
+
+  // Create an array of objects that represent each item, including its id and sum score
+  const items = Object.keys(ratings).map((id) => ({
+    id: id,
+    sumScore: ratings[id][1],
+  }));
+
+  // Sort the array by sum score in descending order
+  items.sort((a, b) => b.sumScore - a.sumScore);
+
+  // Get the top 5 items by taking the first 5 items in the sorted array
+  const topItems = items.slice(0, 5);
+  // Filter the products array to include only the top 5 items
+  const topProductIds = topItems.map(item => item.id);
+  const newtopProductIds = topProductIds.map(str => parseInt(str, 10));
+  console.log(newtopProductIds);
+  const filteredData = data.filter(obj => newtopProductIds.includes(obj.id));
+  console.log("f"+filteredData);
+
+
+  const { filter } = useContext(DataContext)
+  return (
+    <>
+      <h3 className="text-blk heading text-center ">Popular Products</h3>
+      <div className="img_container">
+        {filteredData.map((product) => (
+          <div className="col-md-2 mb-4" key={product.id}>
+            <div className="card h-100 text-center p-4">
+              <img
+                src={product.image}
+                className="card-img-top"
+                alt={product.title}
+                height="250px"
+              />
+              <div className="card-body">
+                <h5 className="card-title mb-0">{product.title}</h5>
+                <p className="card-text lead fw-bold">${product.price}</p>
+                <div></div>
+                <NavLink
+                  to={`/products/${product.id}`}
+                  className="btn btn-outline-dark"
+                >
+                  Buy Now
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+  
+};
+
+export default PopularItems;
