62cdb146baf19c6005465956b63e0b72016ce67e
ShaharBlitzer <blitzer@mail.tau.ac.il>
2023-05-18 16:18:38 +0300

    11

---
+++ src/backend/recommend.py
@@ -0,0 +1,16 @@
+import json
+import pandas as pd
+import numpy as np
+
+def convertJsonToMatrix():
+    file_users = r"C:\TAU\Computer Science\third year\Ecommerce\E---commerce-shop\src\backend\users.json"
+    file_items = r"C:\TAU\Computer Science\third year\Ecommerce\E---commerce-shop\src\backend\item.json"
+    users_df = pd.read_json(file_users)
+    items_df = pd.read_json(file_items)
+    print(users_df,items_df)
+
+
+
+if __name__ == '__main__':
+    convertJsonToMatrix()
+
