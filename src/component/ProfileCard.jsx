1c4deab5b5c975637b75319d1e6f25e29c8a8ae9
Roee Negri <roee.negri@salto.io>
2023-06-24 09:30:52 +0300

    edit profile

--- src/component/ProfileCard.jsx
+++ src/component/ProfileCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import "../App.css";
-import avatar from "../images/roee-n-avatar.png";
 
 function ProfileCard(user) {
 	return (
@@ -14,14 +13,6 @@ function ProfileCard(user) {
 			<p className="normal-text">{user.user.age}</p>
 			<p className="normal-text">Tel Aviv</p>
 			<div className="social-container">
-				<div className="followers">
-					<h1 className="bold-text">15</h1>
-					<h2 className="smaller-text">Followers</h2>
-				</div>
-				<div className="likes">
-					<h1 className="bold-text">27</h1>
-					<h2 className="smaller-text">Likes</h2>
-				</div>
 				<div className="photos">
 					<h1 className="bold-text">6</h1>
 					<h2 className="smaller-text">Products</h2>
